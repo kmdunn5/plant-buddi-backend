a7ac7ed98223208f840ad7319f02b6c740591981
kmdunn5 <kmdunn5@gmail.com>
2021-03-15 14:55:06 -0400

    basic server connection created. DB connection also added.

--- server.js
+++ server.js
@@ -6,3 +6,25 @@ const mongoose = require('mongoose');
 // const cors = require('cors');
 
 
+///////////////////////////
+////     Constants     ////
+///////////////////////////
+const APP = express();
+const PORT = 3003
+const DBNAME = 'plants'
+
+
+///////////////////////////
+//  Mongoose Connection  //
+///////////////////////////
+mongoose.connect(`mongodb://localhost:27017/${DBNAME}`, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+    console.log('connected to mongeese');
+})
+
+///////////////////////////
+////       Routes      ////
+///////////////////////////
+APP.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT} with Kae, Brittani, and Ken`)
+})
