af1f65b7f0477605be28835dfff5a55a538e2d25
kmdunn5 <kmdunn5@gmail.com>
2021-03-15 14:49:38 -0400

    npm init and npm i and requirements added

---
+++ server.js
@@ -0,0 +1,8 @@
+///////////////////////////
+////    Requirements   ////
+///////////////////////////
+const express = require('express');
+const mongoose = require('mongoose');
+// const cors = require('cors');
+
+
