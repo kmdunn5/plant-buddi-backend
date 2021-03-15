e73b8bbd65c964a7fc3ae9ad9bdf060464a2871b
kmdunn5 <kmdunn5@gmail.com>
2021-03-15 15:16:47 -0400

    added the create route for the plants controller

--- controllers/plants.js
+++ controllers/plants.js
@@ -3,14 +3,23 @@
 ///////////////////////////
 const express = require('express');
 const PLANTS = express.Router();
-const Plants = require('../models/Plant')
+const Plant = require('../models/Plant')
 
 
 ///////////////////////////
 ////       Routes      ////
 ///////////////////////////
 PLANTS.get('/', (req, res) => {
-    Plants.find({}, (err, foundPlants) => {
+    Plant.find({}, (err, foundPlants) => {
+        if (err) {
+            res.status(400).json({ error: err.message});
+        }
+        res.status(200).json(foundPlants);
+    });
+});
+
+PLANTS.post('/', (req, res) => {
+    Plants.create(req.body, (err, createdPlant) => {
         if (err) {
             res.status(400).json({ error: err.message});
         }
